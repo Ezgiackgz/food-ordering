84c932d753bc91c89fb5034922f81355f6a5c3ce
Ezgiackgz <ezgisuackgz@gmail.com>
2023-01-29 21:34:56 +0300

    add header carousel and slick slider

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,20 +1,24 @@
 import { useState } from "react";
 import Logo from "../ui/Logo";
-import {FaUser,FaShoppingCart,FaSearch} from "react-icons/fa"; 
+import { FaUser, FaShoppingCart, FaSearch } from "react-icons/fa";
 import Search from "../ui/Search";
 import { GiHamburgerMenu, GiCancel } from "react-icons/gi";
-
+import { useRouter } from "next/router";
 
 const Header = () => {
   const [showModal, setShowModal] = useState(false);
   const [menuModal, setMenuModal] = useState(false);
+  const router = useRouter();
+  console.log(router.asPath)
   return (
-    <div className="h-[5.5rem] bg-secondary">
+    <div className={`h-[5.5rem] z-50 relative ${router.asPath === "/" ? "bg-transparent" : "bg-secondary"}`}>
       <div className="container mx-auto text-white flex justify-between items-center h-full">
         <Logo />
-        <nav className={`sm:static absolute top-0 left-0 sm:w-auto sm:h-auto w-full h-full sm:text-white text-black sm:bg-transparent bg-white sm:flex hidden ${
+        <nav
+          className={`sm:static absolute top-0 left-0 sm:w-auto sm:h-auto w-full h-screen sm:text-white text-black sm:bg-transparent bg-white sm:flex hidden ${
             menuModal === true && "!grid place-content-center"
-          }`}>
+          }`}
+        >
           <ul className="flex gap-x-2 sm:flex-row flex-col">
             <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
               <a href="">Home</a>
@@ -45,19 +49,27 @@ const Header = () => {
           <a href="" className="hover:text-primary transition-all">
             <FaShoppingCart />
           </a>
-          <button href="" className="hover:text-primary transition-all" onClick={()=>setShowModal(true)}>
+          <button
+            href=""
+            className="hover:text-primary transition-all"
+            onClick={() => setShowModal(true)}
+          >
             <FaSearch />
           </button>
           <a href="" className="md:inline-block hidden sm">
             <button className="btn-order">Order Online</button>
           </a>
-          <button className="sm:hidden inline-block" onClick={()=>setMenuModal(true)}>
+          <button
+            className="sm:hidden inline-block"
+            onClick={() => setMenuModal(true)}
+          >
             <GiHamburgerMenu className="text-xl hover:text-primary transition-all" />
           </button>
         </div>
       </div>
-    {showModal &&(<Search setShowModal ={setShowModal} />)}
+      {showModal && <Search setShowModal={setShowModal} />}
     </div>
+
   );
 };
 
