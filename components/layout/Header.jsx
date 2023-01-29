e39da41f59708cd07f02ad4b08a83e72bb83cd51
Ezgiackgz <ezgisuackgz@gmail.com>
2023-01-29 16:46:39 +0300

    search components

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import Logo from "../ui/Logo";
-import {FaUser,FaShoppingCart,FaSearch} from "react-icons/fa"
+import {FaUser,FaShoppingCart,FaSearch} from "react-icons/fa"; 
+import Search from "../ui/Search";
+
 
 const Header = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <div className="h-[5.5rem] bg-secondary">
       <div className="container mx-auto text-white flex justify-between items-center h-full">
@@ -22,21 +26,23 @@ const Header = () => {
             </li>
           </ul>
         </nav>
-        <div className="flex gap-x-4 items-center">
-          <a href="">
+        <div className="flex gap-x-4 items-center ">
+          <a href="" className="hover:text-primary transition-all">
             <FaUser />
           </a>
-          <a href="">
+          <a href="" className="hover:text-primary transition-all">
             <FaShoppingCart />
           </a>
-          <a href="">
+          <button href="" className="hover:text-primary transition-all" onClick={()=>setShowModal(true)}>
             <FaSearch />
-          </a>
+          </button>
+
           <a href="">
             <button className="btn-order">Order Online</button>
           </a>
         </div>
       </div>
+    {showModal &&(<Search setShowModal ={setShowModal} />)}
     </div>
   );
 };
