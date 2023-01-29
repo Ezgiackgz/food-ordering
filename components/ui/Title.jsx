710bfbf9b417d0dbee7d39aa1611156b373fa07d
Ezgiackgz <ezgisuackgz@gmail.com>
2023-01-29 21:35:55 +0300

    add header carousel and slick slider

--- components/ui/Title.jsx
+++ components/ui/Title.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const Title = ({children,addClass}) => {
-  return (
-    <div className={`${addClass}`}>{children}</div>
-  )
-}
+const Title = ({ children, addClass }) => {
+  return <div className={`${addClass}`}>{children}</div>;
+};
 
-export default Title
+export default Title;
