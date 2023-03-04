3871e2e98a1792823fc3be3dcd56081c063fa626
Ezgiackgz <ezgisuackgz@gmail.com>
2023-03-04 22:50:59 +0300

    components add menu

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Header from '@/components/layout/Header';
 import Carousel from '@/components/Carousel';
 import Campaigns from '@/components/Campaigns';
+import MenuWrapper from '@/components/product/MenuWrapper';
 
 
 
@@ -24,6 +25,7 @@ export default function Index() {
       <Carousel />
       </div>
       <Campaigns />
+      <MenuWrapper />
     </div>
     </>
     
